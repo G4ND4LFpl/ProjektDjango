e17a450efa8f8cecb58a7a2fc0053bd646bb47f0
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-06-15 00:21:36 +0200

    15.06 final changes

--- ProjektDjango/settings.py
+++ ProjektDjango/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'ProjektDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'warhammer', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,3 +112,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'myapp/static'),]
