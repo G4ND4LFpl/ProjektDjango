902a5f5b1631a100b690e58a71057a9c5ec8af54
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-06-19 11:17:18 +0200

    last fix

--- ProjektDjango/urls.py
+++ ProjektDjango/urls.py
@@ -6,8 +6,6 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from warhammer import views
-#from django.conf import settings
-#from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -16,4 +14,4 @@ urlpatterns = [
     # Default
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
